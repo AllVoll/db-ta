34497811ebdd53c53a1bd03d8f25fa7536c195da
AllVoll <128129005+AllVoll@users.noreply.github.com>
2023-04-08 10:45:12 +0300

    Add files via upload

---
+++ api-db-ta/migrations/versions/77cfd180406c_initial_migration.py
@@ -0,0 +1,24 @@
+"""initial migration
+
+Revision ID: 77cfd180406c
+Revises: 
+Create Date: 2023-04-06 13:44:36.207971
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '77cfd180406c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
